630d015a8de8e69f3836034eaee00d2970f7d187
yilin <linda.yilin@gmail.com>
2020-03-26 21:23:17 -0500

    sql query from get

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,31 +1,29 @@
+from urllib.parse import unquote
+
+import imp
 import os
 import sys
+import json
 import pymysql
+
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-
-hostname = "localhost"
-username = "cs411sp20team25_team25"
-password = "team25team25"
-database = "cs411sp20team25_team25db"
-
 def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works! Team25!!!\n'
-    version = 'Python %s\n' % sys.version.split()[0]
-    #response = '\n'.join([message, version])
+    body = 'Hello world!\n'
+    if environ['QUERY_STRING']:
+        sql_q = environ['QUERY_STRING'].strip().split('=')[1]
+        db = pymysql.connect(host='localhost', user='cs411sp20team25_team25', passwd='team25team25', db='cs411sp20team25_team25db')
+        curs = db.cursor()
+        try:
+            curs.execute(unquote(sql_q))
+            response = json.dumps([str(d) for d in curs.fetchall()])
+        except:
+            response = 'query error'
+        db.close()
+        body = response.encode()
 
-    db = pymysql.connect(host='localhost', user='cs411sp20team25_team25', passwd='team25team25', db='cs411sp20team25_team25db')
-    curs = db.cursor()
-    query = 'SELECT * FROM Brands'
-    curs.execute(query)
-    response = '\n'.join([str(d) for d in curs.fetchall()])
-    print(response)
-    db.close()
-    #db_version = "Database version : %s " % data
-    #db.close()
-
-    #response = '\n'.join([message, version, db_version])
-
-    return [response.encode()]
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+    start_response(status, headers)
+    return [body]
