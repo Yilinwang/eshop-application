b6e658473a5c0670ae77c70230557620f7cbed60
yilin <linda.yilin@gmail.com>
2020-04-01 17:49:43 -0500

    get select

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,4 +1,4 @@
-from urllib.parse import unquote
+from urllib.parse import unquote, parse_qs
 
 import imp
 import os
@@ -6,22 +6,42 @@ import sys
 import json
 import pymysql
 
+host = 'localhost'
+user = 'cs411sp20team25_team25'
+passwd = 'team25team25'
+db = 'cs411sp20team25_team25db'
+actions = ['select', 'delete', 'update', 'insert']
+
 
 sys.path.insert(0, os.path.dirname(__file__))
 
+
+def get_handler(query):
+    conn = pymysql.connect(host=host, user=user, passwd=passwd, db=db)
+    curs = conn.cursor()
+
+    try:
+        if query['action'][0] == 'select':
+            curs.execute(f"SELECT * FROM {query['table'][0]}")
+
+        response = json.dumps([[str(v) for v in r] for r in curs.fetchall()])
+        conn.commit()
+    except Exception as e:
+        response = f'query error {str(e)}'
+
+    conn.close()
+    return response.encode()
+
+
 def application(environ, start_response):
-    body = 'Hello world!\n'
+    body = 'Hello world!'
     if environ['QUERY_STRING']:
-        sql_q = environ['QUERY_STRING'].strip().split('=')[1]
-        db = pymysql.connect(host='localhost', user='cs411sp20team25_team25', passwd='team25team25', db='cs411sp20team25_team25db')
-        curs = db.cursor()
-        try:
-            curs.execute(unquote(sql_q))
-            response = json.dumps([str(d) for d in curs.fetchall()])
-        except:
-            response = 'query error'
-        db.close()
-        body = response.encode()
+        query = parse_qs(environ['QUERY_STRING'])
+        if 'action' in query and query['action'][0] in actions:
+            body = get_handler(query)
+        else:
+            body = 'invalid action'
+        #body = json.dumps(query['action'])
 
     status = '200 OK'
     headers = [('Content-type', 'text/plain')]
